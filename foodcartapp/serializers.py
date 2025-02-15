18c701ee205a558ccfd368030d6d34120540ca19
AndreyBychenkow <decebell032@yandex.ru>
2025-02-16 00:09:22 +0300

    Send order data to frontend

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -1,24 +1,6 @@
 from rest_framework import serializers
 from phonenumber_field.serializerfields import PhoneNumberField
-
-from django.core.exceptions import ValidationError
 from .models import Order, OrderItem, Product
-
-
-class NameValidator:
-    def __call__(self, value):
-        for char in value:
-            if not (char.isalpha() or char.isspace() or char == '-'):
-                raise ValidationError(
-                    "Может содержать только буквы, пробелы и дефисы")
-
-
-class AddressValidator:
-    def __call__(self, value):
-        for char in value:
-            if not (
-                char.isalpha() or char.isdigit() or char.isspace() or char in '-.,/'):
-                raise ValidationError("Адрес содержит недопустимые символы")
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
@@ -30,8 +12,8 @@ class OrderItemSerializer(serializers.ModelSerializer):
         min_value=1,
         max_value=20,
         error_messages={
-            'min_value': 'Минимальное количество:  1',
-            'max_value': 'Максимальное количество:  20'
+            'min_value': 'Минимальное количество: 1',
+            'max_value': 'Максимальное количество: 20'
         }
     )
 
@@ -39,37 +21,23 @@ class OrderItemSerializer(serializers.ModelSerializer):
         model = OrderItem
         fields = ['product', 'quantity']
 
-    def validate_product(self, value):
-        if not value.is_available:
-            raise serializers.ValidationError("Товар недоступен для заказа")
-        return value
-
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True)
+    items = OrderItemSerializer(
+        many=True,
+        write_only=True
+    )
 
     firstname = serializers.CharField(
         max_length=10,
         min_length=2,
-        trim_whitespace=True,
-        validators=[NameValidator()],
-        error_messages={
-            'min_length': 'Имя должно содержать минимум 2 символа',
-            'max_length': 'Имя не может быть длиннее 10 символов',
-            'blank': 'Имя не может быть пустым'
-        }
+        trim_whitespace=True
     )
 
     lastname = serializers.CharField(
         max_length=20,
         min_length=2,
-        trim_whitespace=True,
-        validators=[NameValidator()],
-        error_messages={
-            'min_length': 'Фамилия должна содержать минимум 2 символа',
-            'max_length': 'Фамилия не может быть длиннее 20 символов',
-            'blank': 'Фамилия не может быть пустой'
-        }
+        trim_whitespace=True
     )
 
     phonenumber = PhoneNumberField(
@@ -79,38 +47,22 @@ class OrderSerializer(serializers.ModelSerializer):
     address = serializers.CharField(
         max_length=200,
         min_length=10,
-        trim_whitespace=True,
-        validators=[AddressValidator()],
-        error_messages={
-            'min_length': 'Адрес должен содержать минимум 10 символов',
-            'max_length': 'Адрес не может быть длиннее 200 символов',
-            'blank': 'Адрес не может быть пустым'
-        }
+        trim_whitespace=True
     )
 
     class Meta:
         model = Order
-        fields = ['firstname', 'lastname', 'phonenumber', 'address', 'items']
-
-    def validate(self, data):
-        items = data.get('items', [])
-
-
-        product_ids = [item['product'].id for item in items]
-        if len(product_ids) != len(set(product_ids)):
-            raise serializers.ValidationError({
-                'items': ['Обнаружены дублирующиеся товары в заказе']
-            })
-
-        return data
+        fields = ['id', 'firstname', 'lastname', 'phonenumber', 'address', 'items']
+        read_only_fields = ['id']
 
     def create(self, validated_data):
         items_data = validated_data.pop('items')
         order = Order.objects.create(**validated_data)
 
-        OrderItem.objects.bulk_create([
+        order_items = [
             OrderItem(order=order, **item_data)
             for item_data in items_data
-        ])
+        ]
+        OrderItem.objects.bulk_create(order_items)
 
         return order
