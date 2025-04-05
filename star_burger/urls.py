fbc9e6bd09ce259ae045dd25ee093dfdb34b8a06
AndreyBychenkow <decebell032@yandex.ru>
2025-04-05 12:00:00 +0300

    add migrations

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path, include
 
-from . import settings
+from star_burger import settings
 
 urlpatterns = [
                   path("admin/", admin.site.urls),
